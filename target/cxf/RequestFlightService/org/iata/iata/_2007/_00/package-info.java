bab27396b3a42c4166b852bf54ef2adb1cc9877d
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2020-07-09 10:16:43 +0100

    drtii-241 remove target from git commit

--- target/cxf/RequestFlightService/org/iata/iata/_2007/_00/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.iata.org/IATA/2007/00", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.iata.iata._2007._00;
